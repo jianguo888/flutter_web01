b225adab2090c268e0973e40cde6aa090e3f79a5
ITmxs <17752170152@163.com>
2021-03-10 14:10:07 +0800

    flutter web

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_mesaage/screens/home/home.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
